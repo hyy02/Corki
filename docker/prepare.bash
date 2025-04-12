b55a08cac714955411734d697a1ef627c83b5b35
Yiyang Huang <huangyiyang24@mails.ucas.ac.cn>
2025-04-12 13:17:19 +0800

    Update prepare.bash

--- docker/prepare.bash
+++ docker/prepare.bash
@@ -2,3 +2,5 @@
 
 cp /home/Corki/docker/10_nvidia.json /etc/glvnd/egl_vendor.d/10_nvidia.json
 cp /home/Corki/docker/merged_config.yaml /data/calvin_debug_dataset/validation/.hydra/merged_config.yaml
+
+cd /home/Corki
