5ced7be6ce1270e342e7b42170103fdce72ee9d8
Yiyang Huang <huangyiyang24@mails.ucas.ac.cn>
2025-04-12 13:35:21 +0800

    Update build_and_run.sh

--- docker/build_and_run.sh
+++ docker/build_and_run.sh
@@ -5,7 +5,7 @@ IMAGE_NAME=corki_calvin
 CONTAINER_NAME=corki_container
 
 # # Build the Docker image
-# docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME .
 
 # Run Docker contiainer with GPU support
 docker run -itd -v ${CORKI_DATA_DIR}:/data -v ${CORKI_MODEL_DIR}:/modelzoo  --name $CONTAINER_NAME --gpus all  -e NVIDIA_DRIVER_CAPABILITIES=all $IMAGE_NAME /bin/bash
